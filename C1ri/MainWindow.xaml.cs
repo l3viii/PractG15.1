da25f534ca75c326c1f9f9128cb27515789e5279
g3teg <g3teg@DESKTOP-SIIESEU>
2021-12-23 22:20:09 +0300

    Добавьте файлы проекта.

---
+++ C1ri/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Gytt1;
+
+namespace C1ri
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Information_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Дроздов Г. ИСП-34", "Разработчик", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void CountClick(object sender, RoutedEventArgs e)
+        {
+
+            int value;
+            bool f = Int32.TryParse(Nperem.Text, out value);
+            if (f == true)
+            {
+                var finding = Practice.ProductEvenWithOut(value);
+                AllNumbers.Text = finding.allNumbers.ToString();
+                Result.Text = finding.calculations;
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное целочисленное значение!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);    
+            }
+        }
+    }
+}
---
+++ Gytt1/Practice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gytt1
+{// МЕЙЙЙЙЙЙЙЙн
+    /// <summary>
+    /// Найти произведение n целых случайных чисел X., распределенных в
+    /// диапазоне от 0 до n.
+    /// </summary>
+    /// <param name="n">Количество чисел и граница генарции</param>
+    /// <returns>Произведение целых случайных чисел и сгенерированные числа </returns>
+    public class Practice
+    {
+        static Random random = new Random();
+        public static (double allNumbers, string calculations) ProductEvenWithOut(int count)
+        {
+            
+            int product = 1;
+            string numbers = string.Empty;
+         
+            for (int i = 0; i < count; i++)
+            {
+                int number = random.Next(0, count);
+
+                numbers += number.ToString() + " ";
+                
+                product *= number;
+            }
+
+            var finding = (product, numbers);
+            return finding;
+        }
+    }
+}
+
